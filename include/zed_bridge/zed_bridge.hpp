fc6fc33598109077843848bf3c100bf4be2a744c
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-05-12 19:03:30 +0100

    fix the bridge code

--- include/zed_bridge/zed_bridge.hpp
+++ include/zed_bridge/zed_bridge.hpp
@@ -3,18 +3,21 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sl/Camera.hpp>
+#include <sensor_msgs/msg/image.hpp>
+
+#define FRAME_ID "zed_camera"
 
 using namespace sl;
 
 class ZedBridge : public rclcpp::Node {
 
     public:
-        ZedBridge(std::string& frame_id);
-        ~ZedBridge();
+        ZedBridge();
 
     private:
         std::string frame_id;
         Camera zed;
+        rclcpp::TimerBase::SharedPtr timer;
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr left_image_pub;
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr right_image_pub;
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr depth_image_pub;
