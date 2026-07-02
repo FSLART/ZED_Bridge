5faaa3ec8f6ba0731eb62e43887f87cb3c168a75
André Lopes <andreglopes2005@gmail.com>
2026-07-02 20:35:08 +0100

    recording the right topics

--- launch/camera_record.launch.py
+++ launch/camera_record.launch.py
@@ -7,34 +7,25 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     
-    # 1. Configuração de Caminhos Básicos
     pkg_share = get_package_share_directory('zed_bridge')
     recorder_config = os.path.join(pkg_share, 'config', 'recorder_config.yaml')
-
-    # 2. Lógica de Criação de Pastas (Pedido do Superior)
-    # Formato: ~/Documents/bags/YYYY/MM/DD/bag_yyyy-mm-dd_hh-mm-ss
     
-    # Obter data e hora atuais
     now = datetime.now()
     
-    # Base: ~/Documents/bags (Expanduser resolve o '~' para /home/user)
+    # Base: ~/Documents/bags 
     base_dir = os.path.expanduser("~/Documents/bags")
     
-    # Criar caminhos: 2025/12/16
     year_str = now.strftime("%Y")
     month_str = now.strftime("%m")
     day_str = now.strftime("%d")
     
-    # Caminho final da pasta do dia
     daily_path = os.path.join(base_dir, year_str, month_str, day_str)
     
     # Criar as pastas fisicamente se não existirem
     os.makedirs(daily_path, exist_ok=True)
     
-    # Nome da Bag: bag_2025-12-16_17-30-00
     bag_name = f"bag_{now.strftime('%Y-%m-%d_%H-%M-%S')}"
     
-    # Caminho completo (URI)
     full_uri = os.path.join(daily_path, bag_name)
 
     print(f"--- CONFIGURAÇÃO DE GRAVAÇÃO ---")
@@ -42,7 +33,6 @@ def generate_launch_description():
     print(f"Nome da Bag: {bag_name}")
     print(f"--------------------------------")
 
-    # 3. Definição do Container
     container = ComposableNodeContainer(
         name='camera_container',
         namespace='',
@@ -50,7 +40,6 @@ def generate_launch_description():
         executable='component_container',
         composable_node_descriptions=[
             
-            # A: ZedBridge
             ComposableNode(
                 package='zed_bridge',
                 plugin='ZedBridge',
